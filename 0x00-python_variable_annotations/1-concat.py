efdb49915aa3cd21852e7607a958189d75d95ee2
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-04 21:01:26 +0300

    Task 1 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module contains a function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two string
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Returns:
+        str: Concatenated string
+    """
+    return str1 + str2
