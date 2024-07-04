4498434cf2b47f1ca77d715e1d2b90bf95960c9f
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-04 21:04:22 +0300

    Task 2 done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module contains function takes float and turns to int
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function takes float and turns to int
+
+    Args:
+        n (float): number to be turned to int
+
+    Returns:
+        int: n rounded down
+    """
+    return int(n)
