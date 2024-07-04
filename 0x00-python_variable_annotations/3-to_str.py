b5cabb5e3d2ee16fea321f16c7c08e92bd51869c
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-04 21:10:08 +0300

    Task 3 and 4 Done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that contains function to_str converts float to str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to_str converts float to str
+
+    Args:
+        n (float): number to convert
+
+    Returns:
+        str: string representation of float
+    """
+    return str(n)
