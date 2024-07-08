ad6a5f8a87897a4a4789515ddae20e9063171b90
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-08 14:57:37 +0300

    Fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,14 +6,14 @@ import asyncio
 from typing import List, Optional
 
 
-wait_random = __import__("0-basic_async_syntax").wait_random
+task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Measures the time it takes to execute n tasks
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     list_delays: list[Optional[float]] = []
     for task in asyncio.as_completed(tasks):
         delay = await task
