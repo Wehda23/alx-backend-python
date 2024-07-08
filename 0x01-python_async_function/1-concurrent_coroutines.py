d31296527d55a490f3fa7f4b222b0b1483e23851
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-08 13:02:38 +0300

    Task 1 done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,8 @@
 """
 A basic implmentation of Asynchronous programming in python
 """
-import asyncio
-import random
 from typing import List, Optional
-
+import asyncio
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
@@ -14,7 +12,9 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A function that waits for a random delay between 0 and max_delay seconds
     """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     list_delays: list[Optional[float]] = []
-    for _ in range(n):
-        list_delays.append(await wait_random(max_delay))
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        list_delays.append(delay)
     return list_delays
