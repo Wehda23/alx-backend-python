e5be9ba828479edc626013f1521daeea7295e59e
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-09 16:03:03 +0300

    Done Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,13 @@ Asynchronous programming in python
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Function that Generates random numbers from 0 to 10
     """
-    for _ in range(10):
-        yield random.uniform(0, 10)
+    for _ in range(0, 10):
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
